3a3c8be48a05f519487169ecd953d06043d6c8ad
Ekim <ekimbasoglu@hotmail.com>
2023-07-10 13:52:19 +0300

    Update server.js

--- MainAPI/server.js
+++ MainAPI/server.js
@@ -61,6 +61,6 @@ app.use('/product', products);
 
 
 // Start the server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
