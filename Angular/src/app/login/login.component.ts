2f7746b822d8fd79b3bcce3a06e3c5670dd8a3c8
Ekim <ekimbasoglu@hotmail.com>
2023-07-18 23:11:20 +0300

    Angular - AuthService updated
    Login endpoint updated

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/authService';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  providers: [AuthService],
 })
 export class LoginComponent {
   loginObj: any = {
     userName: '',
     password: '',
   };
-  constructor(private router: Router) {}
-  onLogin() {
-    if (
-      this.loginObj.userName == 'admin' &&
-      this.loginObj.password == 'admin'
-    ) {
-      localStorage.setItem('loginUserName', this.loginObj.userName);
-      this.router.navigateByUrl('basic-inlineEditing');
-    } else {
-      alert('Wrong Details');
-    }
+  constructor(private router: Router, private authService: AuthService) {}
+  onLogin(email: string, password: string) {
+    this.authService.login(email, password).subscribe(
+      (response) => {
+        // Handle successful login
+        console.log('Login successful');
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        // Handle login error
+        console.error('Login failed', error);
+        alert('Wrong credentials!');
+        // Display error message or perform other actions upon login failure
+      }
+    );
   }
 }
---
+++ Angular/src/app/services/authService.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { User } from '../model/user.model';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000'; // API Endpoint
+  private authToken: string;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http
+      .post(`${this.apiUrl}/user/login`, { email, password })
+      .pipe(
+        tap((response: any) => {
+          const token = response.token;
+          this.cacheToken(token);
+          this.convertTokenToUserModel(token);
+        })
+      );
+  }
+
+  private cacheToken(token: string): void {
+    localStorage.setItem('authToken', token);
+    this.authToken = token;
+  }
+
+  private convertTokenToUserModel(token: string): void {
+    // Assuming you have a User model class
+    // and a method to convert the token to a user model
+    const user = this.convertTokenToUser(token);
+    // Do something with the user model, such as store it in a service or component
+  }
+
+  private convertTokenToUser(token: string): any {
+    const decodedToken: any = this.decodeJwtToken(token);
+    const user: User = new User(
+      decodedToken.user._id,
+      decodedToken.user.name,
+      decodedToken.user.email,
+      decodedToken.user.password,
+      decodedToken.user.surname,
+      decodedToken.user.roles
+    );
+    return user;
+  }
+
+  // Decode the JWT token
+  private decodeJwtToken(token: string): any {
+    try {
+      const decodedToken = jwt_decode(token);
+
+      return decodedToken;
+    } catch (error) {
+      console.error('Error decoding JWT token:', error);
+      return null;
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.authToken;
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    this.authToken = '';
+    this.router.navigate(['/']);
+  }
+
+  register(
+    email: string,
+    password: string,
+    name: string,
+    surname: string
+  ): Observable<any> {
+    const body = { email, password, name, surname };
+
+    return this.http.post(`${this.apiUrl}/user/register`, body);
+  }
+}
