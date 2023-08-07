e7f751e60269273abdc515b0986188a14ab9bc79
Ekim <ekimbasoglu@hotmail.com>
2023-08-07 08:37:37 +0300

    MainAPI - Refactor on User model

--- MainAPI/models/User.js
+++ MainAPI/models/User.js
@@ -6,28 +6,11 @@ const User = new mongoose.Schema({
     name: { type: String, require: true },
     surname: { type: String, require: true },
     password: { type: String, require: true },
-    basket: { type: Array },
-    selectedPaymentMethodId: { type: String },
-    address: { type: String },
     resetPasswordToken: { type: String },
     resetPasswordExpires: { type: Date },
-    roles: { type: String, default: 'User' },
-    preorders: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Preorder'
-    }]
+    roles: { type: String, default: 'User' }
 });
-// const Admin = new Schema({
-//     email: { type: String, require: true },
-//     name: { type: String, require: true },
-//     surname: { type: String, require: true },
-//     password: { type: String, require: true },
-//     roles: "Admin"
-// });
-
 
 const UserModel = mongoose.model('User', User);
-
-// const adminModel = mongoose.model("Admin", Admin);
 
 module.exports = UserModel;
